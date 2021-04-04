755b54933b38d8f9f6f30adcdad6179fd4aa1eac
lianshen <lian.94.shen@gmail.com>
2021-04-03 17:28:54 -0700

    add trading worker

---
+++ Net/CryptoDataIngest/Workers/TradingWorker.cs
@@ -0,0 +1,33 @@
+﻿using CoinbasePro;
+using CryptoDataIngest.Models;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CryptoDataIngest.Workers
+{
+    internal class TradingWorker : BackgroundService
+    {
+        private readonly ILogger<TradingWorker> _logger;
+        private readonly IDataBufferReader<PredictedClose> _bufferIn;
+        private ITradingClient _tradingClient;
+
+        public TradingWorker(
+            ILogger<TradingWorker> logger,
+            IDataBufferReader<PredictedClose> bufferIn)
+        {
+            _logger = logger;
+            _bufferIn = bufferIn;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+
+        }
+    }
+}
